c661263ac817174fec9594e7a168c88920ab21be
Emre İnan <emreinannn@gmail.com>
2024-04-14 23:06:12 +0300

    Add file

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService; //field olarak verdik
+
+        public PttManager(IApplicantService applicantService) //Constructor new yapıldığında çalışır
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            //PersonManager personManager = new PersonManager();
+            //pttmanagerin bağımlı olduğu sınıf yerine o sınıfların interfaccesini yazıyoruz.
+
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ.");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Emre";
+person1.LastName = "İnan";
+person1.DateOfBirthYear = 1993;
+person1.NationalIdentity = 21713422484;
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+
+
+//resharper
+
+
+
